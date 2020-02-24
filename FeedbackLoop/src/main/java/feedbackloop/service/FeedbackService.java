e4ddf9624ab4dda43488a239c7e61eb6fa2a2981
TanskiV <tanskiyv@gmail.com>
2020-02-25 01:27:29 +0200

    Beta

--- FeedbackLoop/src/main/java/feedbackloop/service/FeedbackService.java
+++
@@ -1,9 +0,0 @@
-package feedbackloop.service;
-
-import feedbackloop.dto.FeedbackDto;
-
-public interface FeedbackService {
-	boolean addFeedback (FeedbackDto feedbackDto);
-	FeedbackDto findByMessage (String message);
-
-}
